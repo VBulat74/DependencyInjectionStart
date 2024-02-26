4241ffb0e3b223a5eb3fe70f614075df312b5ebc
VB <Vladimir.Bulat@gmail.com>
2024-02-26 14:40:13 +0300

    Lesson_08 (Custom Builder, BindsInstance)

--- app/src/main/java/ru/com/bulat/dependencyinjectionstart/example2/di/AppComponent.kt
+++ app/src/main/java/ru/com/bulat/dependencyinjectionstart/example2/di/AppComponent.kt
@@ -1,10 +1,24 @@
 package ru.com.bulat.dependencyinjectionstart.example2.di
 
+import android.content.Context
+import dagger.BindsInstance
 import dagger.Component
 import ru.com.bulat.dependencyinjectionstart.example2.presentation.MainActivity
 
-@Component (modules = [DataModule::class, DomainModule::class, ContextModule::class])
+@Component (modules = [DataModule::class, DomainModule::class])
 interface AppComponent {
 
     fun inject (activity : MainActivity)
+
+    @Component.Builder
+    interface AppComponentBuilder {
+
+        @BindsInstance
+        fun context(context : Context) : AppComponentBuilder
+
+        @BindsInstance
+        fun timeMillis(timeMillis: Long) : AppComponentBuilder
+
+        fun build() : AppComponent
+    }
 }
--- app/src/main/java/ru/com/bulat/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/ru/com/bulat/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -3,7 +3,6 @@ package ru.com.bulat.dependencyinjectionstart.example2.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dependencyinjectionstart.R
-import ru.com.bulat.dependencyinjectionstart.example2.di.ContextModule
 import ru.com.bulat.dependencyinjectionstart.example2.di.DaggerAppComponent
 import javax.inject.Inject
 
@@ -11,18 +10,22 @@ class MainActivity : AppCompatActivity() {
 
     private val component by lazy {
         DaggerAppComponent.builder()
-            .contextModule(ContextModule(this))
+            .context(this)
+            .timeMillis(System.currentTimeMillis())
             .build()
     }
+
+
 
     @Inject
     lateinit var viewModel : ExampleViewModel
 
     override fun onCreate(savedInstanceState : Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         component.inject(this)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         viewModel.method()
     }
