c01568dfb6d05bd5d22c40e6791c0cda000b343b
VB <Vladimir.Bulat@gmail.com>
2024-02-27 09:51:27 +0300

    Lesson_12 (Provider)

--- app/src/main/java/ru/com/bulat/dependencyinjectionstart/example2/di/AppComponent.kt
+++ app/src/main/java/ru/com/bulat/dependencyinjectionstart/example2/di/AppComponent.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import dagger.BindsInstance
 import dagger.Component
 import ru.com.bulat.dependencyinjectionstart.example2.presentation.MainActivity
+import ru.com.bulat.dependencyinjectionstart.example2.presentation.MainActivity2
 
 @AppScope
 @Component(modules = [DataModule::class, DomainModule::class, ViewModelModule::class])
 interface AppComponent {
 
     fun inject(activity : MainActivity)
+    fun inject(activity : MainActivity2)
 
     @Component.Factory
     interface AppComponentFactory {
--- app/src/main/java/ru/com/bulat/dependencyinjectionstart/example2/presentation/MainActivity2.kt
+++ app/src/main/java/ru/com/bulat/dependencyinjectionstart/example2/presentation/MainActivity2.kt
@@ -1,15 +1,13 @@
 package ru.com.bulat.dependencyinjectionstart.example2.presentation
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.dependencyinjectionstart.R
 import ru.com.bulat.dependencyinjectionstart.example2.ExampleApp
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
 
     @Inject
     lateinit var viewModelFactory : ViewModelFactory
@@ -35,11 +33,5 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.method()
         viewModel2.method()
-
-        findViewById<TextView>(R.id.tv_hello_world).setOnClickListener {
-            Intent(this, MainActivity2::class.java).apply {
-                startActivity(this)
-            }
-        }
     }
 }
